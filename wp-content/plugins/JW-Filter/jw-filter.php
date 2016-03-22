ab8b35889da587efc97390fb4bcc4e456862a567
hasanmbstu13 <hasanmbstu13@gmail.com>
2016-03-22 10:27:39 +0600

    add filter hook successfully.

--- wp-content/plugins/JW-Filter/jw-filter.php
+++ wp-content/plugins/JW-Filter/jw-filter.php
@@ -7,3 +7,14 @@ Author: Mainul Hasan
 Author URI: http://net.tutsplus.com
 Version: 1.0
 */
+
+// Here first one is what we are filtering
+// // second one is custom defined method
+// add_filter('the_title','jw_modify_title_cb');
+// We can also use anonymous function
+add_filter('the_title', function($content) {
+	return ucwords($content);
+});
+
+// More simpler
+add_filter('the_title',ucwords);
