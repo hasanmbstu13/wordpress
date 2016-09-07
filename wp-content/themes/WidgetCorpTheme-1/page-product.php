4e8345546d02bd5ea70b470cbedf8addf6162515
hasanmbstu13 <hasanmbstu13@gmail.com>
2016-09-07 10:27:14 +0600

    Fixes the broken link of images

--- wp-content/themes/WidgetCorpTheme-1/page-product.php
+++ wp-content/themes/WidgetCorpTheme-1/page-product.php
@@ -8,13 +8,15 @@
 <!-- Wordpress recognize a page as a template by writng Template Name in comment -->
 <!-- Product Page template will parse those page where the template page is product page -->
 
+<?php $template_url = get_template_directory_uri() ?>
 <?php get_header(); the_post(); ?>
 
 <div id='main-content'>
 	<h2 class="product-title"><?php  the_title(); ?></h2>
     <div class="product-info-box">
     <?php //echo get_post_meta($post->ID, 'product_large', true) ?>
-        <img src="<?php echo get_post_meta($post->ID, 'product_large', true); ?>" />            
+		<?php $image_src = $template_url.get_post_meta($post->ID, 'product_large', true); ?>
+        <img src="<?php echo $image_src; ?>" />            
         <ul>
         	<!-- get_post_meta has three parameters which post which custom field exactly true or false
         	if true just return the first value if multiple value has return first value if false then return an array -->
